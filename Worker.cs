8a7b45e51459d0bb7ac3510f793aa452eff5d447
Scrumlord123 <94675757+Scrumlord123@users.noreply.github.com>
2023-05-29 23:54:32 +0200

    Updated RecieveBid and ValidateBid methods

--- Worker.cs
+++ Worker.cs
@@ -3,7 +3,7 @@ using RabbitMQ.Client.Events;
 using System.Text;
 using Newtonsoft.Json;
 using MongoDB.Driver;
-using System;
+using System.Threading;
 using MongoDB.Bson;
 using ServiceWorker.DTO;
 using Microsoft.Extensions.Hosting;
@@ -40,98 +40,120 @@ public class Worker : BackgroundService
 
 
         //Connects to the database
-       MongoClient client = new MongoClient("mongodb+srv://mikkelbojstrup:aha64jmj@auktionshus.67fs0yo.mongodb.net/");
+        MongoClient client = new MongoClient("mongodb+srv://mikkelbojstrup:aha64jmj@auktionshus.67fs0yo.mongodb.net/");
         _database = client.GetDatabase("Auction");
         AuctionCollection = _database.GetCollection<Auction>("AuctionCollection");
         UsersCollection = _database.GetCollection<UserDTO>("UsersCollection");
         BidCollection = _database.GetCollection<BidDTO>("BidCollection");
 
-         //Logs the enviroment variable
+        //Logs the enviroment variable
         _logger.LogInformation($"File path is set to : {_docPath}");
         _logger.LogInformation($"RabbitMQ connection is set to : {_rabbitMQ}");
         _logger.LogInformation($"MongoDB er sat til: {_config.GetConnectionString("MongoDB")}");
-        
+
     }
 
-    public void RecieveBid()
-    {
-        _logger.LogInformation("Recieving bid at {time}", DateTime.Now);
-        var factory = new RabbitMQ.Client.ConnectionFactory() { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-        {
-            channel.QueueDeclare(queue: "bidQueue",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+    //var factory = new RabbitMQ.Client.ConnectionFactory() { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += async (model, ea) =>
+    public void RecieveBid()
+{
+    var factory = new RabbitMQ.Client.ConnectionFactory() { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
+    factory.DispatchConsumersAsync = true;
+
+    using var connection = factory.CreateConnection();
+    using var channel = connection.CreateModel();
+    {
+        channel.QueueDeclare(queue: "bidQueue",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+        
+        channel.QueueBind(queue: "bidQueue",
+                          exchange: "bidExchange",
+                          routingKey: "bid");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.Received += async (model, ea) =>
+        {
+            try
             {
                 _logger.LogInformation("Bid received at {time}", DateTime.Now);
+
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
 
-                //Deserialize message
+                // Deserialize message
                 BidDTO? bid = JsonConvert.DeserializeObject<BidDTO>(message);
 
                 if (bid != null)
                 {
-                    _logger.LogInformation("Auction {auctionId} recieved bid from {bidderId} at {bidTime} for {bidAmount}", bid.AuctionId, bid.BidderId, bid.BidTime, bid.BidAmount);
+                    _logger.LogInformation("Auction {auctionId} received bid from {bidderId} at {bidTime} for {bidAmount}", bid.AuctionId, bid.BidderId, bid.BidTime, bid.BidAmount);
+
                     await validateBid(bid.Id, bid.BidAmount, bid.BidderId, bid.BidTime, bid.AuctionId);
+                    _logger.LogInformation("Bid was validated and processed");
                 }
                 else
                 {
                     _logger.LogInformation("Failed to deserialize bid");
                 }
-                _logger.LogInformation("Data recieved: {message}", message);
-            };
+                _logger.LogInformation("Data received: {message}", message);
 
-            channel.BasicConsume(queue: "bidQueue",
-                                 autoAck: true,
-                                 consumer: consumer);
+                // Acknowledge the message
+                channel.BasicAck(ea.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error receiving bid");
+            }
+        };
 
+        channel.BasicConsume(queue: "bidQueue",
+                             autoAck: false,
+                             consumer: consumer);
+
+        // Wait for the consumer to finish processing messages
+        while (consumer.IsRunning)
+        {
+            Thread.Sleep(100);
         }
     }
+}
+
 
     public async Task validateBid(string id, decimal bidAmount, string bidderId, DateTime bidTime, string auctionId)
+{
+    var filter = Builders<Auction>.Filter.Eq(a => a.Id, auctionId);
+    var update = Builders<Auction>.Update
+        .Set(a => a.CurrentBid, bidAmount)
+        .Set(a => a.User, new UserDTO { Id = bidderId });
+
+    var result = await AuctionCollection.UpdateOneAsync(filter, update);
+
+    if (result.ModifiedCount > 0)
     {
-        Auction auction = AuctionCollection.Find(a => a.Id == auctionId).FirstOrDefault();
-
-        var client = new MongoClient(_config.GetConnectionString("MongoDB"));
-        var database = client.GetDatabase("MongoDB:Database");
-        var collection = _database.GetCollection<BidDTO>(_config["MongoDB:BidCollection"]);
-        var auctionCollection = _database.GetCollection<Auction>(_config["MongoDB:AuctionCollection"]);
-
-        var auctionById = auctionCollection.Find(a => a.Id == auctionId).FirstOrDefault();
         var bid = new BidDTO(id, bidAmount, bidderId, bidTime, auctionId);
-
-        if (bid.BidAmount > auction.CurrentBid)
-        {
-            auction.CurrentBid = bid.BidAmount;
-            auction.User = new UserDTO();
-            auction.User.Id = bid.BidderId;
-            auctionCollection.ReplaceOne(a => a.Id == auctionId, auction);
-            collection.InsertOne(bid);
-            _logger.LogInformation("Bid was accepted");
-        }
-        else
-        {
-            _logger.LogInformation("Bid was too low");
-        }
+        BidCollection.InsertOne(bid);
+        _logger.LogInformation("Bid was accepted");
     }
+    else
+    {
+        _logger.LogInformation("Bid was too low");
+    }
+}
+
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        RecieveBid();
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            RecieveBid();
             await Task.Delay(1000, stoppingToken);
         }
     }
-    
+
 
 }
 

461e68c97f4549e7308c71cbe8e2c1cce8cae550
Scrumlord123 <94675757+Scrumlord123@users.noreply.github.com>
2023-05-30 22:35:27 +0200

    Fully working ServiceWorker

--- Worker.cs
+++ Worker.cs
@@ -4,160 +4,167 @@ using System.Text;
 using Newtonsoft.Json;
 using MongoDB.Driver;
 using System.Threading;
-using MongoDB.Bson;
+using System.Threading.Tasks;
 using ServiceWorker.DTO;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 
-namespace ServiceWorker;
-
-public class Worker : BackgroundService
+namespace ServiceWorker
 {
-    private readonly ILogger<Worker> _logger;
-    private readonly string? _docPath;
-    private readonly string? _rabbitMQ;
-    private readonly IMongoDatabase _database;
-    private IConfiguration _config;
-    private IMongoDatabase Database { get; }
-    private IMongoCollection<Auction> AuctionCollection;
-    private IMongoCollection<BidDTO> BidCollection;
-    private IMongoCollection<UserDTO> UsersCollection;
-
-    public Worker(ILogger<Worker> logger, IConfiguration config)
+    public class Worker : BackgroundService
     {
-        //Takes enviroment variable and sets it to the logger
-        _logger = logger;
-        _docPath = config["DocPath"];
-        _rabbitMQ = config["RabbitMQ"];
-        _config = config;
+        private readonly ILogger<Worker> _logger;
+        private readonly string? _docPath;
+        private readonly string? _rabbitMQ;
+        private readonly IMongoDatabase _database;
+        private IConfiguration _config;
+        private IMongoDatabase Database { get; }
+        private IMongoCollection<Auction> AuctionCollection;
+        private IMongoCollection<BidDTO> BidCollection;
+        private IMongoCollection<UserDTO> UsersCollection;
+        private IConnection _connection;
+        private IModel _channel;
 
-        //Retrieves host name and IP address from the current enviroment
-        var hostName = System.Net.Dns.GetHostName();
-        var ips = System.Net.Dns.GetHostAddresses(hostName);
-        var _ipaddress = ips.First().MapToIPv4().ToString();
-
-
-        //Connects to the database
-        MongoClient client = new MongoClient("mongodb+srv://mikkelbojstrup:aha64jmj@auktionshus.67fs0yo.mongodb.net/");
-        _database = client.GetDatabase("Auction");
-        AuctionCollection = _database.GetCollection<Auction>("AuctionCollection");
-        UsersCollection = _database.GetCollection<UserDTO>("UsersCollection");
-        BidCollection = _database.GetCollection<BidDTO>("BidCollection");
-
-        //Logs the enviroment variable
-        _logger.LogInformation($"File path is set to : {_docPath}");
-        _logger.LogInformation($"RabbitMQ connection is set to : {_rabbitMQ}");
-        _logger.LogInformation($"MongoDB er sat til: {_config.GetConnectionString("MongoDB")}");
-
-    }
-
-    //var factory = new RabbitMQ.Client.ConnectionFactory() { Uri = new Uri("amqp://guest:guest@localhost:5672/") };
-
-    public IModel RecieveBid()
-{
-    var factory = new RabbitMQ.Client.ConnectionFactory() { HostName = "localhost" };
-    factory.DispatchConsumersAsync = true;
-
-    using var connection = factory.CreateConnection();
-    using var channel = connection.CreateModel();
-    {
-        channel.QueueDeclare(queue: "bidQueue",
-                             durable: false,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
-        
-        channel.QueueBind(queue: "bidQueue",
-                          exchange: "bidExchange",
-                          routingKey: "bid");
-
-        var consumer = new AsyncEventingBasicConsumer(channel);
-        consumer.Received += async (model, ea) =>
+        public Worker(ILogger<Worker> logger, IConfiguration config)
         {
-            try
-            {
-                _logger.LogInformation("Bid received at {time}", DateTime.Now);
+            // Initialiserer logger og læser konfigurationsoplysninger
+            _logger = logger;
+            _docPath = config["DocPath"];
+            _rabbitMQ = config["RabbitMQ"];
+            _config = config;
 
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
+            // Opretter forbindelse til MongoDB og initialiserer MongoDB-samlinger
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = System.Net.Dns.GetHostAddresses(hostName);
+            var _ipaddress = ips.First().MapToIPv4().ToString();
+            MongoClient client = new MongoClient("mongodb+srv://mikkelbojstrup:aha64jmj@auktionshus.67fs0yo.mongodb.net/");
+            _database = client.GetDatabase("Auction");
+            AuctionCollection = _database.GetCollection<Auction>("AuctionCollection");
+            UsersCollection = _database.GetCollection<UserDTO>("UsersCollection");
+            BidCollection = _database.GetCollection<BidDTO>("BidCollection");
 
-                // Deserialize message
-                BidDTO? bid = JsonConvert.DeserializeObject<BidDTO>(message);
-
-                if (bid != null)
-                {
-                    _logger.LogInformation("Auction {auctionId} received bid from {bidderId} at {bidTime} for {bidAmount}", bid.AuctionId, bid.BidderId, bid.BidTime, bid.BidAmount);
-
-                    await validateBid(bid.Id, bid.BidAmount, bid.BidderId, bid.BidTime, bid.AuctionId);
-                    _logger.LogInformation("Bid was validated and processed");
-                }
-                else
-                {
-                    _logger.LogInformation("Failed to deserialize bid");
-                }
-                _logger.LogInformation("Data received: {message}", message);
-
-                // Acknowledge the message
-                channel.BasicAck(ea.DeliveryTag, false);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error receiving bid");
-            }
-        };
-
-        channel.BasicConsume(queue: "bidQueue",
-                             autoAck: false,
-                             consumer: consumer);
-
-        // Wait for the consumer to finish processing messages
-        while (consumer.IsRunning)
-        {
-            Thread.Sleep(100);
+            // Logger konfigurationsoplysningerne
+            _logger.LogInformation($"File path is set to : {_docPath}");
+            _logger.LogInformation($"RabbitMQ connection is set to : {_rabbitMQ}");
+            _logger.LogInformation($"MongoDB er sat til: {_config.GetConnectionString("MongoDB")}");
         }
-        return consumer as IModel;
-    }
-   
-}
 
-
-    public async Task validateBid(string id, decimal bidAmount, string bidderId, DateTime bidTime, string auctionId)
-{
-    var filter = Builders<Auction>.Filter.Eq(a => a.Id, auctionId);
-    var update = Builders<Auction>.Update
-        .Set(a => a.CurrentBid, bidAmount)
-        .Set(a => a.User, new UserDTO { Id = bidderId });
-
-    var result = await AuctionCollection.UpdateOneAsync(filter, update);
-
-    if (result.ModifiedCount > 0)
-    {
-        var bid = new BidDTO(id, bidAmount, bidderId, bidTime, auctionId);
-        BidCollection.InsertOne(bid);
-        _logger.LogInformation("Bid was accepted");
-    }
-    else
-    {
-        _logger.LogInformation("Bid was too low");
-    }
-}
-
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
-        //Out of scope, hvis den ikke bliver placeret her ift .NET
-        var consumer = RecieveBid();
-
-        while (!stoppingToken.IsCancellationRequested)
+        public void ConnectRabbitMQ()
         {
-            await Task.Delay(1000, stoppingToken);
+            // Opretter en forbindelse og en kanal til RabbitMQ
+            var factory = new RabbitMQ.Client.ConnectionFactory() { HostName = "localhost" };
+            factory.DispatchConsumersAsync = true;
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            // Erklærer en kø og binder den til en udveksling med en rute
+            _channel.QueueDeclare(queue: "bidQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueBind(queue: "bidQueue", exchange: "bidExchange", routingKey: "bid");
+
+            // Opretter en forbruger, der lytter på meddelelser i køen
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            consumer.Received += async (model, ea) =>
+            {
+                try
+                {
+                    _logger.LogInformation("Bid received at {time}", DateTime.Now);
+
+                    // Modtager og deserialiserer budmeddelelsen
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    BidDTO? bid = JsonConvert.DeserializeObject<BidDTO>(message);
+
+                    if (bid != null)
+                    {
+                        _logger.LogInformation("Auction {auctionId} received bid from {bidderId} at {bidTime} for {bidAmount}", bid.AuctionId, bid.BidderId, bid.BidTime, bid.BidAmount);
+
+                        // Validerer og behandler budet
+                        await ValidateBid(bid.Id, bid.BidAmount, bid.BidderId, bid.BidTime, bid.AuctionId);
+                        _logger.LogInformation("Bid was validated and processed");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("Failed to deserialize bid");
+                    }
+
+                    _logger.LogInformation("Data received: {message}", message);
+
+                    // Bekræfter modtagelsen af budmeddelelsen
+                    _channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error receiving bid");
+                }
+            };
+
+            // Begynder at forbruge budmeddelelser fra køen
+            _channel.BasicConsume(queue: "bidQueue", autoAck: false, consumer: consumer);
+        }
+
+        public async Task ValidateBid(string id, decimal bidAmount, string bidderId, DateTime bidTime, string auctionId)
+        {
+            // Finder auktionsdokumentet
+            var auction = await AuctionCollection.Find(a => a.Id == auctionId).FirstOrDefaultAsync();
+
+            if (auction == null)
+            {
+                _logger.LogInformation("Auction not found");
+                return;
+            }
+
+            if (bidAmount <= auction.CurrentBid)
+            {
+                // Budet er for lavt
+                _logger.LogInformation("Bid was too low");
+                return;
+            }
+
+            // Opdaterer auktionsdokumentet med det nye bud
+            var filter = Builders<Auction>.Filter.Eq(a => a.Id, auctionId);
+            var update = Builders<Auction>.Update
+                .Set(a => a.CurrentBid, bidAmount)
+                .Set(a => a.User, new UserDTO { Id = bidderId });
+
+            var result = await AuctionCollection.UpdateOneAsync(filter, update);
+
+            if (result.IsAcknowledged && result.ModifiedCount > 0)
+            {
+                // Indsætter budet i BidCollection
+                var bid = new BidDTO(id, bidAmount, bidderId, bidTime, auctionId);
+                BidCollection.InsertOne(bid);
+                _logger.LogInformation("Bid was accepted");
+            }
+            else
+            {
+                _logger.LogInformation("Bid was rejected");
+            }
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            // Opretter forbindelse til RabbitMQ og begynder at lytte efter budmeddelelser
+            ConnectRabbitMQ();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            // Afslutter forbindelsen og kanalen til RabbitMQ ved afslutning af tjenesten
+            _channel?.Dispose();
+            _connection?.Dispose();
+
+            await base.StopAsync(cancellationToken);
         }
     }
-
-
 }
+
 
